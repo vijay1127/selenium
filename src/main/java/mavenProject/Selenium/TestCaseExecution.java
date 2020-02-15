e366e180a8e8888dbda5495b1389c764a55241e1
Satish <Satish@Satish-PC>
2020-02-15 07:39:43 +0530

    comment in TestCaseExecution

--- src/main/java/mavenProject/Selenium/TestCaseExecution.java
+++ src/main/java/mavenProject/Selenium/TestCaseExecution.java
@@ -1,6 +1,8 @@
 package mavenProject.Selenium;
 
 public class TestCaseExecution {
+	
+	//comment to commit in git
 
 	public void Column2Value1() {
 		System.out.println("This is the test case for Column2Value1");
